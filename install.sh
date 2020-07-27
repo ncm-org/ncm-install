3f80eedc96a265e67734145af0804e9273ae581b
myroid <myroid@foxmail.com>
2020-07-27 14:45:54 +0800

    feat: install ncm

--- install.sh
+++ install.sh
@@ -5,74 +5,99 @@
 
 os_name=
 arch_name=
+download_name=
 latest_version=
+
 ncm_folder=$HOME/.ncm
 
 function echo_green() {
-    echo -e "\033[32m${1}\033[0m"
+	echo -e "\033[32m${1}\033[0m"
 }
 
 function echo_red() {
-    echo -e "\033[31m${1}\033[0m"
+	echo -e "\033[31m${1}\033[0m"
 }
 
 function get_os_name() {
-    os_name="$(uname | tr '[:upper:]' '[:lower:]')"
+	os_name="$(uname | tr '[:upper:]' '[:lower:]')"
 }
 
 function get_arch_name() {
-    case $(uname -m) in
-    i386) arch_name="386" ;;
-    i686) arch_name="386" ;;
-    x86_64) arch_name="amd64" ;;
-    esac
+	case $(uname -m) in
+	i386) arch_name="386" ;;
+	i686) arch_name="386" ;;
+	x86_64) arch_name="amd64" ;;
+	esac
 }
 
 function get_latest_version() {
-    curl -LkSs "https://api.github.com/repos/ncm-org/ncm/releases/latest" -o "$ncm_folder/latest"
-    if [ ! -f "$ncm_folder/latest" ]; then
+	curl -LkSs "https://api.github.com/repos/ncm-org/ncm/releases/latest" -o "$ncm_folder/latest"
+	if [ ! -f "$ncm_folder/latest" ]; then
 		echo_red "failed to get version information, please try again!"
-        exit 1
-    fi
+		exit 1
+	fi
 
-    latest_version=$(grep tag_name "$ncm_folder/latest" | awk -F '[:,"v]' '{print $6}')
-    rm -f "$ncm_folder/latest"
+	latest_version=$(grep tag_name "$ncm_folder/latest" | awk -F '[:,"v]' '{print $6}')
+	if [ -z "$latest_version" ]; then
+		echo_red "failed to get version information, please try again!"
+		exit 1
+	fi
+
+	rm -f "$ncm_folder/latest"
+}
+
+function download_latest_version() {
+	download_name="ncm_${latest_version}_${os_name}_${arch_name}.zip"
+	echo_green "download $download_name"
+
+	curl -Lk "https://github.com/ncm-org/ncm/releases/download/v$latest_version/$download_name" -o "$ncm_folder/$download_name"
+	if [ ! -f "$ncm_folder/$download_name" ]; then
+		echo_red "download failed, please try again!"
+		exit 1
+	fi
+}
+
+function unzip_latest_version() {
+	unzip -qq -o "$ncm_folder/$download_name" -d "$ncm_folder"
+	rm -f "$ncm_folder/$download_name"
+
+	if [ ! -f "$ncm_folder/ncm" ]; then
+		echo_red "no NCM was found, please try again!"
+		exit 1
+	fi
+}
+
+function set_envionment_variables() {
+	export NCM_HOME="$ncm_folder"
+	export PATH="$NCM_HOME":"$PATH"
+
+	ncm_enc=$(printf "\n# added by ncm-install\nexport NCM_HOME=%s\nexport PATH=\$NCM_HOME:\$PATH\n" "$ncm_folder")
+
+	if [[ -f "$HOME"/.zshrc ]] && [[ "$(grep -c NCM_HOME <"$HOME"/.zshrc)" == 0 ]]; then
+		echo "$ncm_enc" >>"$HOME"/.zshrc
+	fi
+
+	if [[ -f "$HOME"/.bashrc ]] && [[ "$(grep -c NCM_HOME <"$HOME"/.bashrc)" == 0 ]]; then
+		echo "$ncm_enc" >>"$HOME"/.bashrc
+	fi
+
+	if [[ -f "$HOME"/.bash_profile ]] && [[ "$(grep -c NCM_HOME <"$HOME"/.bash_profile)" == 0 ]]; then
+		echo "$ncm_enc" >>"$HOME"/.bash_profile
+	fi
+
+	chmod +x "$ncm_folder"/ncm
 }
 
 function install() {
-    mkdir -p "${ncm_folder}"
+	mkdir -p "${ncm_folder}"
 
-    get_os_name
-    get_arch_name
-    get_latest_version
+	get_os_name
+	get_arch_name
+	get_latest_version
+	download_latest_version
+	unzip_latest_version
 
-    download_name="ncm_${latest_version}_${os_name}_${arch_name}.zip"
-    echo_green "download $download_name"
-
-    curl -Lk "https://github.com/ncm-org/ncm/releases/download/v$latest_version/$download_name" -o "$ncm_folder/$download_name"
-    if [ ! -f "$ncm_folder/$download_name" ]; then
-		echo_red "download failed, please try again!"
-        exit 1
-    fi
-
-    unzip -qq -o "$ncm_folder/$download_name" -d "$ncm_folder"
-    rm -f "$ncm_folder/$download_name"
-
-    export NCM_HOME="$ncm_folder"
-    export PATH="$NCM_HOME":"$PATH"
-
-    ncm_enc=$(printf "\n# added by ncm-install\nexport NCM_HOME=%s\nexport PATH=\$NCM_HOME:\$PATH\n" "$ncm_folder")
-
-    if [[ -f "$HOME"/.zshrc ]] && [[ "$(grep -c NCM_HOME <"$HOME"/.zshrc)" == 0 ]]; then
-        echo "$ncm_enc" >>"$HOME"/.zshrc
-    elif [[ -f "$HOME"/.bashrc ]] && [[ "$(grep -c NCM_HOME <"$HOME"/.bashrc)" == 0 ]]; then
-        echo "$ncm_enc" >>"$HOME"/.bashrc
-    elif [[ -f "$HOME"/.bash_profile ]] && [[ "$(grep -c NCM_HOME <"$HOME"/.bash_profile)" == 0 ]]; then
-        echo "$ncm_enc" >>"$HOME"/.bash_profile
-    fi
-
-    chmod +x "$ncm_folder"/ncm
-    echo_green "successfully installed ncm@$latest_version"
+	echo_green "successfully installed ncm@$latest_version"
 }
 
 install
